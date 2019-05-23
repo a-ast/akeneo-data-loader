0b5f9523dbd7985f79dff2870e638423fc52ccec
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-05-23 18:12:10 +0200

    Add standard upsert (for APIs that implement upsert interface).

---
+++ src/Upsert/StandardUpserter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Aa\AkeneoDataLoader\Upsert;
+
+use Akeneo\Pim\ApiClient\Api\Operation\UpsertableResourceInterface;
+
+class StandardUpserter implements Upsertable
+{
+    /**
+     * @var UpsertableResourceInterface
+     */
+    private $api;
+
+    public function __construct(UpsertableResourceInterface $api)
+    {
+        $this->api = $api;
+    }
+
+    public function upsert(array $data)
+    {
+        $code = $data['identifier'] ?? $data['code'];
+
+        $this->api->upsert($code, $data);
+    }
+}
