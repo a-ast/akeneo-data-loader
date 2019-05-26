7abcef6061406de97c86c28d7ff328f9e4470f00
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-05-26 17:57:36 +0200

    Implement channeling batch generator.

---
+++ spec/Iterator/ChannelingBatchGeneratorSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace spec\Aa\AkeneoDataLoader\Iterator;
+
+use Aa\AkeneoDataLoader\Iterator\ChannelingBatchGenerator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ChannelingBatchGeneratorSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(2, 'id');
+    }
+
+    function it_batches()
+    {
+        $input = [
+            ['id' => 'a', 'value' => 1],
+            ['id' => 'b', 'value' => 2],
+            ['id' => 'b', 'value' => 3],
+            ['id' => 'c', 'value' => 4],
+            ['id' => 'a', 'value' => 5],
+            ['id' => 'b', 'value' => 6],
+            ['id' => 'a', 'value' => 7],
+            ['id' => 'a', 'value' => 8],
+        ];
+
+        $this->getBatches($input)->shouldYield([
+
+            [
+                ['id' => 'b', 'value' => 2],
+                ['id' => 'b', 'value' => 3],
+            ],
+
+            [
+                ['id' => 'a', 'value' => 1],
+                ['id' => 'a', 'value' => 5],
+            ],
+
+            [
+                ['id' => 'a', 'value' => 7],
+                ['id' => 'a', 'value' => 8],
+            ],
+
+            [
+                ['id' => 'c', 'value' => 4],
+            ],
+
+            [
+                ['id' => 'b', 'value' => 6],
+            ],
+        ]);
+    }
+}
---
+++ src/Iterator/ChannelingBatchGenerator.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Aa\AkeneoDataLoader\Iterator;
+
+use Traversable;
+
+class ChannelingBatchGenerator
+{
+    /**
+     * @var int
+     */
+    private $size;
+
+    /**
+     * @var string
+     */
+    private $channel;
+
+    public function __construct(int $size, string $channel)
+    {
+        $this->size = $size;
+        $this->channel = $channel;
+    }
+
+    public function getBatches(iterable $iterator): Traversable
+    {
+        $batches = [];
+
+        foreach ($iterator as $item) {
+
+            $code = $item[$this->channel];
+
+            $batches[$code][] = $item;
+
+            foreach ($batches as $batchCode => $batch) {
+
+                if (count($batch) < $this->size) {
+                    continue;
+                }
+
+                yield $batch;
+
+                unset($batches[$batchCode]);
+            }
+        }
+
+
+        foreach ($batches as $batch) {
+
+            yield $batch;
+        }
+    }
+}
